89df74053f49311daff8d88d47e58da19efd8eb6
Anatoliy <anartius@gmail.com>
2022-07-09 22:55:49 +0200

    Finish of the stage 4/7.

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -11,24 +11,49 @@ fun main() {
 
 fun getFlashCards() : List<Flashcard> {
     val flashcards = mutableListOf<Flashcard>()
+    val n = getNumberOfCards()
+    var term: String
+    var termExists: Boolean
+    var definition: String
+    var definitionExists: Boolean
 
-    println("Input the number of cards:")
-    val n = try {
-        readln().toInt()
-    } catch (e:NumberFormatException) {
-        0
-    }
-
-    var front: String
-    var back: String
     for (i in 0 until n) {
+
         println("Card #${i + 1}:")
-        front = readln()
+        while (true) {
+            termExists = false
+            term = readln()
+            flashcards.forEach { if (it.term == term) termExists = true }
+            if (!termExists) {
+                break
+            } else println("The term \"$term\" already exists. Try again:")
+        }
+
         println("The definition for card #${i + 1}:")
-        back = readln()
-        flashcards.add(Flashcard(front, back))
+        while (true) {
+            definitionExists = false
+            definition = readln()
+            flashcards.forEach {
+                if (it.definition == definition) definitionExists = true
+            }
+            if (!definitionExists) {
+                break
+            } else {
+                println("The definition \"$definition\" already exists. Try again.")
+            }
+        }
+
+        flashcards.add(Flashcard(term, definition))
     }
     return flashcards.toList()
+}
+
+
+fun getNumberOfCards() : Int {
+    println("Input the number of cards:")
+    return try {
+        readln().toInt()
+    } catch (e:NumberFormatException) { 0 }
 }
 
 
@@ -36,10 +61,22 @@ fun checkAnswers(flashcards: List<Flashcard>) {
     for (i in flashcards.indices) {
         println("Print the definition of \"${flashcards[i].term}\":")
         val answer = readln()
-        println(
-            if (flashcards[i].definition == answer) {
-                "Correct!"
-            } else "Wrong. The right answer is \"${flashcards[i].definition}\"."
-        )
+        var alternativeTerm = ""
+
+
+        if (flashcards[i].definition == answer) {
+            println("Correct!")
+        } else {
+            flashcards.forEach {
+                if (it.definition == answer) alternativeTerm = it.term
+            }
+
+            val endOfString = if (alternativeTerm.isNotEmpty()) {
+                ", but your definition is correct for \"$alternativeTerm\"."
+            } else "."
+
+            println("Wrong. The right answer is \"${flashcards[i].definition}\"" +
+                    endOfString)
+        }
     }
 }
